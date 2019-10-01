d9b54bb415548868b0a6c851332395c9f84a58f7
Benana42 <54997866+Benana42@users.noreply.github.com>
2019-10-01 12:30:27 -0400

    Optional Distance
    Added difficulty boolean. When true it will write each floor tile as a distance along the path to the starting point.

--- Culminating Game/Maze.py
+++ Culminating Game/Maze.py
@@ -1,6 +1,6 @@
 from random import randint, choice; from time import time
 
-def generate(width, height, doTimer=False):
+def generate(width, height, doTimer=False, difficulty=True):
     if doTimer: startTime = time()              # If time == True it'll track how long the generation takes
     # Creates a 2D list of 1's with the point (1,1) as a 0
     mapList     = [[1 for y in range(height)] for x in range(width)]
@@ -9,7 +9,10 @@ def generate(width, height, doTimer=False):
         new = choice(frontiers)                 # Takes a random frontier bordering a floor tile
         frontiers.remove(new)                   # Removes the random frontier
         x, y = new[0]; i, j = new[1]            # Takes the 2 tiles that were picked from the frontiers
-        if mapList[x][y] == 1: mapList[x][y] = new[2] + 1; mapList[i][j] = 0    # If the tile is a wall, set the 2 tiles to be floor
+        if mapList[x][y] == 1:
+            if difficulty:  mapList[x][y] = new[2] + 1
+            else:           mapList[x][y] = 0
+            mapList[i][j] = 0
         for a, b in [(-2, 0), (2, 0), (0, -2), (0, 2)]:     # Add these to the tiles to get their neighbours
             if x + a >= 0 and x + a < width and y + b >= 0 and y + b < height:  # Makes sure that the neighbours aren't out of the list
                 # If the neighbour is a wall then set it and the connecting piece to be frontiers
@@ -17,4 +20,4 @@ def generate(width, height, doTimer=False):
     if doTimer: print("Time Elapsed: {}".format(round(time() - startTime, 20))) # Print the time from the start of the generation and the end
     return mapList
 
-if __name__ == '__main__': print("\n".join(" ".join(" # " if y == 1 else "{:^3}".format(str(y)) for y in x) for x in generate(39, 39, doTimer=True)))
+if __name__ == '__main__': print("\n".join(" ".join(" # " if y == 1 else "{:^3}".format(str(y)) for y in x) for x in generate(39, 39, doTimer=True, difficulty=False)))
